b97164c311907d6f5d18174122b7f6beb0ff77dd
Pera Pisar <1405223@gmail.com>
2019-08-19 20:41:22 +0200

    Update packages.

--- releases/release.sh
+++ releases/release.sh
@@ -6,7 +6,7 @@ rm -rf wikia-nitro-*
 rm -rf *.zip
 # Generate all builds.
 echo "Generating new builds..."
-electron-packager .. --all --electron-version=4.1.0
+electron-packager .. --all --electron-version=6.0.2
 # Windows.
 echo "Packaging Windows builds..."
 zip -qr win32-ia32 wikia-nitro-win32-ia32
